e1f2f6c0a25f74d6b81598a643ebe5bdbc51ae24
Eric Goldschmidt <eric@macbook-pro.mynetworksettings.com>
2023-09-27 22:19:09 -0400

    push

--- app/components/Dev.tsx
+++ app/components/Dev.tsx
@@ -1,9 +1,13 @@
-import React, { Component } from "react";
+'use client'
+import React, { Component, useState } from "react";
 import Slider from "react-slick";
 import Head from "next/head";
 import Image from 'next/image'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import { Button, Modal } from 'flowbite-react';
+
+
 <Head>
   <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
@@ -12,6 +16,15 @@ import "slick-carousel/slick/slick-theme.css";
 type Props = {}
 
 function Dev({}: Props) {
+
+  const [openEventurous, setOpenEventurous] = useState<string | undefined>();
+  const [openSubway, setOpenSubway] = useState<string | undefined>();
+  const [openGlimpse, setOpenGlimpse] = useState<string | undefined>();
+  const [openLettr, setOpenLettr] = useState<string | undefined>();
+  const [openRB, setOpenRB] = useState<string | undefined>();
+  const [openRosie, setOpenRosie] = useState<string | undefined>();
+  const props = { openEventurous, setOpenEventurous, openSubway, setOpenSubway, openGlimpse, setOpenGlimpse, openLettr, setOpenLettr, openRB, setOpenRB, openRosie, setOpenRosie };
+
   var settings = {
     dots: true,
     infinite: true,
@@ -50,7 +63,7 @@ function Dev({}: Props) {
       <div className='scroll-pt-2 mx-3 gap-y-3 slider-container px-3' id='development'>
         <h1 className='text-2xl text-gold uppercase -ml-3'>Development Projects</h1>
         <Slider {...settings}>
-        <div>
+        <div onClick={() => props.setOpenGliimpse('default')} className="cursor-pointer">
             <Image
               src='/resources/cardGlimpse.png'
               width={240}
@@ -59,7 +72,7 @@ function Dev({}: Props) {
               className="p-3"
             />
           </div>
-          <div>
+          <div onClick={() => props.setOpenEventurous('default')} className="cursor-pointer">
             <Image
               src='/resources/cardEventurous.png'
               width={240}
@@ -67,8 +80,9 @@ function Dev({}: Props) {
               alt="Eventurous Card"
               className="p-3"
             />
+          
           </div>
-          <div>
+          <div onClick={() => props.setOpenLettr('default')} className="cursor-pointer">
           <Image
               src='/resources/cardLettr.png'
               width={240}
@@ -77,7 +91,7 @@ function Dev({}: Props) {
               className="p-3"
             />
           </div>
-          <div>
+          <div onClick={() => props.setOpenSubway('default')} className="cursor-pointer">
           <Image
               src='/resources/cardSubway.png'
               width={240}
@@ -86,7 +100,7 @@ function Dev({}: Props) {
               className="p-3"
             />
           </div>
-          <div>
+          <div onClick={() => props.setOpenRosie('default')} className="cursor-pointer">
           <Image
               src='/resources/cardRosie.png'
               width={240}
@@ -95,7 +109,7 @@ function Dev({}: Props) {
               className="p-3"
             />
           </div>
-          <div>
+          <div onClick={() => props.setOpenRB('default')} className="cursor-pointer">
           <Image
               src='/resources/cardRB.png'
               width={240}
@@ -105,6 +119,23 @@ function Dev({}: Props) {
             />
           </div>
         </Slider>
+        
+      <Modal show={props.openEventurous === 'default'} onClose={() => props.setOpenEventurous(undefined)} className="h-screen relative">
+      
+        <Modal.Body className="absolute h-[calc(100vh-2rem)] w-full overflow-scroll">
+          <Image
+              src='/resources/cardRB.png'
+              fill={true}
+              alt="Eventurous Card"
+              className="p-3 object-contain h-96"
+            />
+            <Button color="gray" onClick={() => props.setOpenEventurous(undefined)} className="absolute top-0 right-0 rounded-full ">
+              X
+            </Button>
+        </Modal.Body>
+        
+        
+      </Modal>
       </div>
   
   )
