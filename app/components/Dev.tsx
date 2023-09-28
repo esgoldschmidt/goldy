be25ff5a46ef5ce23aa04c70ecaab38207285eec
Eric Goldschmidt <eric@macbook-pro.mynetworksettings.com>
2023-09-27 21:25:45 -0400

    slider projects

--- app/components/Dev.tsx
+++ app/components/Dev.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import Head from "next/head";
+import Image from 'next/image'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 <Head>
@@ -17,22 +18,21 @@ function Dev({}: Props) {
     arrows: true, 
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
+          slidesToScroll: 1,
         }
       },
       {
@@ -50,23 +50,59 @@ function Dev({}: Props) {
       <div className='scroll-pt-2 mx-3 gap-y-3 slider-container px-3' id='development'>
         <h1 className='text-2xl text-gold uppercase -ml-3'>Development Projects</h1>
         <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+        <div>
+            <Image
+              src='/resources/cardGlimpse.png'
+              width={240}
+              height={312}
+              alt="Glimpse Card"
+              className="p-3"
+            />
           </div>
           <div>
-            <h3>2</h3>
+            <Image
+              src='/resources/cardEventurous.png'
+              width={240}
+              height={312}
+              alt="Eventurous Card"
+              className="p-3"
+            />
           </div>
           <div>
-            <h3>3</h3>
+          <Image
+              src='/resources/cardLettr.png'
+              width={240}
+              height={312}
+              alt="Lettr Card"
+              className="p-3"
+            />
           </div>
           <div>
-            <h3>4</h3>
+          <Image
+              src='/resources/cardSubway.png'
+              width={240}
+              height={312}
+              alt="Eventurous Card"
+              className="p-3"
+            />
           </div>
           <div>
-            <h3>5</h3>
+          <Image
+              src='/resources/cardRosie.png'
+              width={240}
+              height={312}
+              alt="Eventurous Card"
+              className="p-3"
+            />
           </div>
           <div>
-            <h3>6</h3>
+          <Image
+              src='/resources/cardRB.png'
+              width={240}
+              height={312}
+              alt="Eventurous Card"
+              className="p-3"
+            />
           </div>
         </Slider>
       </div>
